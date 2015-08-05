a26b87d8ea4dfed885a17b9b37dc9588e0087e08
lindaheriksson <linda.h.eriksson@gmail.com>
2015-08-05 11:53:38 +0200

    metod

--- metod.py
+++ metod.py
@@ -1,1 +1,15 @@
-metod.py
+# def metod(): print "hej"
+
+# metod()
+
+# def metod(): return "hej"
+
+# print metod()
+
+# def metod(message): print message
+
+# metod("hej")
+
+def metod(): return "hej"
+
+print metod()
