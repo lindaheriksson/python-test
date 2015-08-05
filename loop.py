50b4d0f7df1426713af373f61fec5050b7fbef96
lindaheriksson <linda.h.eriksson@gmail.com>
2015-08-05 14:49:05 +0200

    loop and program update

---
+++ loop.py
@@ -0,0 +1,8 @@
+#coding:UTF-8
+items = [1, 2, 5]
+for item in items:
+	print item
+
+dictionary = {"nyckel1":u"värde1","nyckel2":u"värde2"}
+for key, value in dictionary.items():
+	print key + "/" + value
--- program.py
+++ program.py
@@ -3,25 +3,53 @@
 
 #print robot["price"]*robot["count"]*robot["vat"]+book["price"]*book["count"]*book["vat"]
 
-class Product(object):
-	price = 0
-	count = 0
-	vat = 0
+#---
+
+# class Product(object):
+# 	price = 0
+# 	count = 0
+# 	vat = 0	
+
+# class Product(object):
+# 	def __init__(self, price, count, vat):
+# 		self.price = price
+# 		self.count = count
+# 		self.vat = vat 
+# 	def price_with_vat(self):
+# 		return self.price*self.count*self.vat
+			
+
+# robot = Product(price=900, count=2, vat=1.25)
+# book = Product(price=100, count=1, vat=1.06)
+
+
+
+# print robot.price_with_vat()+book.price_with_vat()
+
+#--
+
 
 class Product(object):
+	def __init__(self, price, count, vat):
+		self.price = price
+		self.count = count
+		self.vat = vat 
 	def price_with_vat(self):
-		return self.price * self.count * self.vat
+		return self.price*self.count*self.vat
+	
 
-robot = Product()
-robot.price = 900
-robot.count = 2
-robot.vat = 1.25
+products = [
+	Product(price=900,count=2,vat=1.25), 
+	Product(price=100, count=1, vat=1.06)
+]
 
-book = Product()
-book.price = 100
-book.count = 1
-book.vat = 1.06
+total_price = products[0].price_with_vat() + products[1].price_with_vat()
 
-print robot.price_with_vat()+book.price_with_vat()
+print total_price
+
+
+
+
+
 
 
